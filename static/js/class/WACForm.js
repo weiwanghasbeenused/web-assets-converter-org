edb4f9dfb354bfdaa9f0f1f2ba3526bde4dbf5c5
wei <wei@o-r-g.com>
2025-04-12 12:00:52 +0800

    debug

--- static/js/class/WACForm.js
+++ static/js/class/WACForm.js
@@ -122,13 +122,16 @@ export default class WACForm{
     handleListResult(result){
         this.isFetching = false;
         const fetched = result['data'];
+        console.log('>>> handleListResult');
         if(fetched.length === 0 ) {
+            console.log('    fetched.length === 0');
             if(this.num_fetched === 0)
                 this.itemsContainer.innerHTML = `<div class="wac-message-container">${this.messages['no-item']}</div>`;  
             this.fetchedAll = true;
         } else {
             const resetList = this.num_fetched === 0;
             this.updateList(fetched, resetList);
+            console.log('    ' + fetched.length + ' vs ' + this.num_per_fetch);
             if(fetched.length < this.num_per_fetch) this.fetchedAll = true;
         }
         this.form.setAttribute('data-fetched-all', this.fetchedAll ? '1' : '0');
